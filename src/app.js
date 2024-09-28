ce6df98603755f6489c6f070c8209ab9973f1375
anderson <anderson_oliveira@unochapeco.edu.br>
2024-09-28 11:37:15 -0300

    criando

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//Conecta ao banco
+mongoose.connect('mongodb+srv://andersonoliveira:gpPVn6Ew7UU8Dzsr@easyfood.e1vby.mongodb.net/?retryWrites=true&w=majority&appName=easyfood');
+
+//Carrega os Models
+const Product = require('./models/product');
+
 
 //carregar as rotas
 const indexRoute = require('./routes/index-route');
