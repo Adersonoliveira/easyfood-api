48e59180f3cd8802769c66f37d4f442816c8dc48
Anderson <133705146+Adersonoliveira@users.noreply.github.com>
2024-12-03 09:05:10 -0300

    ajuste

--- src/routes/payment-route.js
+++ src/routes/payment-route.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/paymentController');
 
-// Realizar pagamento
 router.post('/process', paymentController.processPayment);
 
 module.exports = router;
